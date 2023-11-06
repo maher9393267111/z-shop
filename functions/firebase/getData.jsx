2c82bcb2176094b35229fc320a938b58d410d84a
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-11-06 20:43:19 +0300

    format

--- functions/firebase/getData.jsx
+++ functions/firebase/getData.jsx
@@ -1,30 +1,32 @@
 import {
-    DocumentData,
-    collection,
-    doc,
-    getCountFromServer,
-    getDoc,
-    getDocs,
-    query,
-    where,
-    deleteDoc,
-    limit,
-  } from "firebase/firestore";
-  //import { db, storage } from "./index";
-  import {db,storage} from '.'
-  import { ref, deleteObject , getDownloadURL ,uploadBytes } from "firebase/storage";
-  // import { v4 as uuidv4 } from 'uuid';
-
+  DocumentData,
+  collection,
+  doc,
+  getCountFromServer,
+  getDoc,
+  getDocs,
+  query,
+  where,
+  deleteDoc,
+  limit,
+} from "firebase/firestore";
+//import { db, storage } from "./index";
+import { db, storage } from ".";
+import {
+  ref,
+  deleteObject,
+  getDownloadURL,
+  uploadBytes,
+} from "firebase/storage";
+// import { v4 as uuidv4 } from 'uuid';
 
 //step-1- get number of documents in one Collection
-  export const getCount = async(col)=>{
-    //col --> {products , categories , subcategories}
-const colRef = collection(db , col)
-const snapshot = await getCountFromServer(colRef)
-return snapshot.data().count;
-
-
-  };
+export const getCount = async (col) => {
+  //col --> {products , categories , subcategories}
+  const colRef = collection(db, col);
+  const snapshot = await getCountFromServer(colRef);
+  return snapshot.data().count;
+};
 
 function postToJSON(doc) {
   const data = doc.data();
@@ -32,130 +34,96 @@ function postToJSON(doc) {
   return {
     ...data,
     id: doc.id,
-// createdAt: data.createdAt..... === true or  false
+    // createdAt: data.createdAt..... === true or  false
     // createdAt: data.createdAt?.toMillis() || 0,
     // updatedAt: data.updatedAt?.toMillis() || 0,
   };
 }
 
-  // 3nde so2el m3yan 3la instock aw collection motghayira hasab price product name, 
-  //review bade 3dad m3ayn mnil data 
-  // query and limmit is params
-export const getDocuments = async (col , querydata = null , limit = null) =>{
+// 3nde so2el m3yan 3la instock aw collection motghayira hasab price product name,
+//review bade 3dad m3ayn mnil data
+// query and limmit is params
+export const getDocuments = async (col, querydata = null, limit = null) => {
   const queryConstraints = [];
   // query here is value from function {price === 200} or {name === adidas} ....
   if (querydata !== null) queryConstraints.push(where(...querydata));
   if (limit !== null) queryConstraints.push(limit(limit));
   //col motghayir marra category marra sub category marra production
-  
-// where(..) , where(''') , where ....
-  const ref = collection(db ,col)
-    // query here is method from firebase to filter data
-  const docsRef = query(ref ,...queryConstraints);
- //  if query params is exist and limit is exist  filter data else show all data from collection
- const documents = (await getDocs(docsRef)).docs.map(postToJSON);
 
+  // where(..) , where(''') , where ....
+  const ref = collection(db, col);
+  // query here is method from firebase to filter data
+  const docsRef = query(ref, ...queryConstraints);
+  //  if query params is exist and limit is exist  filter data else show all data from collection
+  const documents = (await getDocs(docsRef)).docs.map(postToJSON);
 
- return documents; //  [] array of data named categories 
- //, products  , subcategories in /admin/.../all
-
+  return documents; //  [] array of data named categories
+  //, products  , subcategories in /admin/.../all
 };
-
-
-
 
 // -----------------------------
 
-
 // get Single item from firestore ==> ... product ,subcategory , category , review ...
-
 
 //col  is collection changeable  , id is id of doucment to get from firestore changeble
 
+export const getDocument = async (col, id) => {
+  const document = postToJSON(await getDoc(doc(db, col, id)));
 
-export const getDocument = async (col ,id)=>{
-
-
-  const document = postToJSON(await getDoc(doc(db,col , id)))
-
-
-  return document
-
-}
+  return document;
+};
 
 // col is collection name -->  category  or product
 // item is  object that have  data
-export const  handleDelete  = (col ,item ,isproduct=false) =>{
-
-
+export const handleDelete = (col, item, isproduct = false) => {
   // path of document we well delete
-  const itemDoc = doc(db, col ,item.id)
-// delete document from firestore then delete document images
-  deleteDoc(itemDoc).then(async()=>{
+  const itemDoc = doc(db, col, item.id);
+  // delete document from firestore then delete document images
+  deleteDoc(itemDoc).then(async () => {
+    if (isproduct) {
+      deleteImages(item?.images);
+    } else {
+      // specifu image folder and name to delete it => col is folder  image.name is image name will deleted from storage
+      const desertRef = ref(storage, `${col}/${item.image.name}`);
 
-    if(isproduct) {
-
-      deleteImages(item?.images)
-
+      await deleteObject(desertRef);
     }
 
-    else {
-
-
-    // specifu image folder and name to delete it => col is folder  image.name is image name will deleted from storage
-    const desertRef = ref(storage ,`${col}/${item.image.name}`)
-
-
-await deleteObject(desertRef)
-
-    }
-
-
-
-
-window.location.reload()
-
-  })
-
-}
-
+    window.location.reload();
+  });
+};
 
 // ------------------------
 
-
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 export const uploadImages = async (files) => {
-    const urls = []
-    await Promise.all(files.map(async (file) => {
-        const imageRef = ref(storage, uuidv4())
-        const res = await uploadBytes(imageRef, file)
-        const url = await getDownloadURL(res.ref)
-        urls.push(url)
-    }))
-  
-    return urls
+  const urls = [];
+  await Promise.all(
+    files.map(async (file) => {
+      const imageRef = ref(storage, uuidv4());
+      const res = await uploadBytes(imageRef, file);
+      const url = await getDownloadURL(res.ref);
+      urls.push(url);
+    })
+  );
+
+  return urls;
+};
+
+export const deleteImages = async (images) => {
+  try {
+    const deleteResponses = await Promise.all(
+      images.map((image) => deleteObject(ref(storage, image)))
+    );
+    return deleteResponses;
+  } catch (error) {
+    throw error;
   }
-  
-  
-  export const deleteImages = async (images) => {
-    try {
-      
-      const deleteResponses = await Promise.all(
-        images.map((image) => deleteObject(ref(storage, image)))
-      );
-      return deleteResponses;
-    } catch (error) {
-      throw error;
-    }
-  };
-  
-  
-  
-  
-  export const antdFieldValidation = [
-    {
-      required: true,
-      message: "This field is required",
-    },
-  ];
-  
+};
+
+export const antdFieldValidation = [
+  {
+    required: true,
+    message: "This field is required",
+  },
+];
